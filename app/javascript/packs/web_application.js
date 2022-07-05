bbbbfb51300a1b03eabf33f620d6b0c78d947de4
amysid <ammadsid87@gmail.com>
2022-07-05 21:42:42 +0530

    error

--- app/javascript/packs/web_application.js
+++ app/javascript/packs/web_application.js
@@ -2,7 +2,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery-mousewheel")
 // import the bootstrap javascript module
 // import "bootstrap"
 
