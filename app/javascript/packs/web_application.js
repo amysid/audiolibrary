da2b1cd26af5a5af17bf118e8e58c270d753fb78
amysid <ammadsid87@gmail.com>
2022-07-05 21:13:36 +0530

    error

--- app/javascript/packs/web_application.js
+++ app/javascript/packs/web_application.js
@@ -8,7 +8,7 @@ require("channels")
 // import the application.scss we created for the bootstrap CSS (if you are not using assets stylesheet)
 // import "../stylesheets/application"
 var jQuery = require('jquery')
-
+import $ from './jquery';
 // include jQuery in global and window scope (so you can access it globally)
 // in your web browser, when you type $('.div'), it is actually refering to global.$('.div')
 global.$ = global.jQuery = jQuery;
